79db59f8c1acbaebbe75fc9e49933f23bab3d416
Shrewbitz <swxg@umich.edu>
2021-01-10 18:09:01 -0500

    first

---
+++ bbcurrencyexchange.js
@@ -0,0 +1,183 @@
+class Exchange {
+    constructor(exchangeArr) {
+        //[[top, bottom, rate]]
+        this.startCountry = {}
+        if (Array.isArray(exchangeArr)) {
+            this.addMany(exchangeArr)
+        }
+
+        this.cache = {}
+        this.interferenceCache = {}
+    }
+
+    find(top, bottom) {
+        let answer = null
+        if (this.startCountry[top].endCountry[bottom])  {
+            answer = this.startCountry[country].endCountry[bottom]
+        } else if (this.cache[top+bottom]) {
+            console.log('Used cache to find the answer!')
+            answer = this.cache[top+bottom]
+        } else {
+            let queue = [top, 1, []]
+            let path = null
+            let set = new Set()
+            set.add(top)
+            while (queue.length) {
+                let country = queue.shift()
+                let rate = queue.shift()
+                path = queue.shift()
+                
+                if (this.startCountry[country].endCountry[bottom]) {
+                    path.push(country+bottom)    
+                    rate *= this.startCountry[country].endCountry[bottom] 
+                    answer = this.decimalRound(rate)
+                    break
+                } else {
+                    if (this.startCountry[country]) {
+                        let keys = Object.keys(this.startCountry[country].endCountry)
+                        for (let i = 0; i < keys.length; i++) {
+                            if (set.has(keys[i])) continue
+                            set.add(keys[i])
+                            path.push(country+keys[i])
+                            queue.push(keys[i], rate*this.startCountry[country].endCountry[keys[i]], [...path])
+                            path.pop()
+                        }
+                    }
+                }
+            }
+            this.cache[top+bottom] = answer
+            this.updateInterferenceCache(top,bottom,path)
+            console.log(path)
+        }
+        console.log(`1 ${top} is worth ${answer} ${bottom}`)
+        return answer
+    } 
+
+    updateOne(top, bottom, rate) {
+        this.addToStartCountry(top,bottom,rate)
+        this.deleteCacheValue(top+bottom)
+        this.deleteCacheValue(bottom+top)
+    }
+
+    updateInterferenceCache(top, bottom, path) {
+        for (let i = 0; i < path.length; i++) {
+            if (this.interferenceCache[path[i]]) {
+             this.interferenceCache[path[i]].push(top+bottom)
+            } else {
+             this.interferenceCache[path[i]] = [top+bottom] 
+            }
+         }
+    }
+
+    deleteCacheValue(name) {
+        if (this.interferenceCache[name]) {
+            let cacheDeleteArr = this.interferenceCache[name]
+            for (let i = 0; i < cacheDeleteArr.length; i++) {
+                this.cache[cacheDeleteArr[i]] = null
+                console.log(`deleted ${cacheDeleteArr[i]} from the cache`)
+            }
+            this.interferenceCache[name] = null
+        }
+    }
+
+    addOne(top, bottom, rate) {
+        this.addToStartCountry(top,bottom,rate) 
+        console.log(`Added ${top} to ${bottom} and their inverse.`)
+    }
+
+    addMany(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            this.addToStartCountry(arr[i][0],arr[i][1],arr[i][2])
+        }
+        console.log(`Added ${arr.length} new exchanges along with their inverses.`)
+    }
+
+    addToStartCountry(top,bottom,rate) {
+        if (!this.startCountry[top]) this.startCountry[top] = new endCountry()
+        if (!this.startCountry[bottom]) this.startCountry[bottom] = new endCountry()
+        
+        this.startCountry[top].endCountry[bottom] = this.decimalRound(rate) 
+        this.startCountry[bottom].endCountry[top] = this.decimalRound(1/rate) 
+    }
+
+    decimalRound(number) {
+        return Math.round(number * 1000) / 1000
+    }
+}
+
+class endCountry {
+    constructor() {
+        this.endCountry = {}
+    }
+}
+
+
+let x = new Exchange([['USD','JPY', 103],['USD','SEK', 10],['USD','RUB', 91],['EUR','AUD', 1.5],['GBP','JPY', 141],['GBP','EUR', 1.1]])
+x.addOne("EUR",'CNY', 8)
+x.find('CNY', 'USD')
+x.updateOne('USD', 'JPY', 10)
+x.find('CNY', 'USD')
+
+x.find('CNY', 'USD')
+// console.log(x.startCountry['USD'].endCountry['JPY'])
+// console.log(x.startCountry["JPY"].endCountry['GBP'])
+/*
+
+
+list of countries
+    list of countries for each country pointing to rate
+sek swedish
+rub russian
+aud australia
+gbp england
+krw korea
+inr india
+THB thai
+1         
+USD -> JPY 103
+USD -> SEK 10
+USD -> RUB 91
+EUR -> AUD 1.5
+GBP -> JPY 141
+GBP -> EUR 1.1
+EUR -> CNY 8
+EUR -> KRW 1337
+JPY -> KRW 10.5
+CNY -> INR 11
+THB -> INR 2.5
+
+
+        
+EXAMPLE DATA:
+        If we want to .find(USD, JPY) this is what the cache will lool like after
+
+        startCountry =  {
+
+            USD: 
+                endCountry = {
+                    CNY: 10 <-----
+                    CAD: 2
+                }
+
+            CNY: 
+                endCountry = {
+                    JPY: 4 <-----
+                    AUD: 12
+                    USD: .1
+                }
+        }
+        
+
+        cache = {
+            USDJPY: 40 
+        }
+        
+
+        interferenceCache = {
+            USDCNY: [USDJPY]
+            CNYJPY: [USDJPY]
+        }
+        
+
+
+*/ 
---
+++ bbnodehops.js
@@ -0,0 +1,214 @@
+class HopCombo {
+    constructor() {
+        this.matrix = new Matrix
+        this.matrix.createMatrixSizeN(10)
+        this.matrix.setStartRowColumn(0,0)
+        this.start = this.matrix.start
+    }
+
+    dfsApproach(start, hops) {
+        let total = 0
+
+        function dfs(node, h) {
+            if (h === 0) {
+                total++
+                return
+            }
+            let dirs = [node.left, node.right, node.up, node.down]
+            for (let i = 0; i < dirs.length; i++) {
+                if (dirs[i]) dfs(dirs[i], h-1)
+            }
+        }
+
+        dfs(start, hops)
+        return total
+    }
+
+    dfsApproachUnique(start, hops) {
+
+        let set = new Set()
+        
+        function dfs(node, h, str) {
+            if (h === 0) {
+                if (!set.has(str)){
+                    set.add(str)
+                }
+                return
+            }
+            let dirs = [node.left, node.right, node.up, node.down]
+            for (let i = 0; i < dirs.length; i++) {
+                if (dirs[i]) dfs(dirs[i], h-1, str+node.val)
+            }
+        }
+
+        dfs(start, hops, "")
+        return set.size
+    }
+
+    memoizationApproach(start, hops) {
+        /*
+         in a matrix you would just use location and hops as the key in the memo, but we have nodes. One idea is to make a value in the node 
+         that takes care of that. But lets pretend we cant modify the nodes. I then have to make a location parameters in the dfs(r c)
+        */
+        let memo = {}
+
+        function dfs(node, h, r, c, str) {
+            let total = 0
+            if (h === 0) {
+                memo[r+"x"+c+'x'+h] = total
+                return 1
+            }
+            if (memo[r+"x"+c+'x'+h]) return memo[r+"x"+c+'x'+h]
+            
+                if (node.left) total += dfs(node.left, h-1, r, c-1)
+                if (node.right) total += dfs(node.right, h-1, r, c+1)
+                if (node.up) total += dfs(node.up, h-1, r-1, c)
+                if (node.down) total += dfs(node.down, h-1, r+1, c)
+
+
+
+            memo[r+"x"+c+'x'+h] = total
+            return total
+        }
+        console.log(memo)
+        return dfs(start, hops, 0, 0, )
+     
+    }
+
+
+    dpApproach(start, hops) {
+        // we build a new matrix for each hop. As we iterate through the points we 
+        // add up the neighbors of the old matrix and set the currents value to it
+        let length = 0
+        let cur = start
+        while (cur) {
+            cur = cur.right
+            length++
+        }
+        console.log(length)
+        let arr = []
+        for (let i = 0; i < length; i++) {
+            arr.push([])
+            for (let j = 0; j < length; j++) {
+                arr[i].push(0)
+            }
+        }
+
+        arr[0][0] = 1
+        
+        while (hops) {
+            let nextArr = []
+            for (let i = 0; i < length; i++) {
+                nextArr.push([])
+                for (let j = 0; j < length; j++) {
+                    let total = 0
+                    if (j > 0) total += arr[i][j-1]
+                    if (j < length-1) total += arr[i][j+1]
+                    if (i > 0) total += arr[i-1][j]
+                    if (i < length-1) total += arr[i+1][j]
+                    nextArr[i].push(total)
+                }
+                
+            }
+            arr = nextArr
+            hops--
+        }
+
+        let answer = 0
+        for (let i = 0; i < length; i++) {
+            
+            for (let j = 0; j < length; j++) {
+                answer+= arr[i][j]
+            }
+            
+        }
+        return answer
+
+    }
+
+    mathApproach() {
+
+    }
+
+}
+
+
+////////////////////////////////////////////////////////
+
+class Matrix {
+    constructor() {
+        this.matrix = null
+        this.nodeMatrix = null
+        this.start = null
+    }
+
+    createMatrixSizeN(n) {
+        let arr = []
+        let narr = []
+        for (let i = 0; i < n; i++) {
+            arr.push([])
+            narr.push([])
+            for (let j = 0; j < n; j++) {
+                arr[i].push(Math.floor(Math.random()*7))
+                narr[i].push(new Node(arr[i][j]))
+            }
+        }
+        
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+                let node = narr[i][j]
+                // console.log(narr[i])
+                // for (let k = 0; k < 4; k++) {
+                    if (j > 0) node.left = narr[i][j-1]
+                    if (j < n-1) node.right = narr[i][j+1]
+                    if (i > 0) node.up = narr[i-1][j]
+                    if (i < n-1) node.down = narr[i+1][j]
+                // } 
+            }
+        }
+        this.matrix = arr
+        this.nodeMatrix = narr
+    }
+
+    setStartRowColumn(r,c) {
+        let n = this.nodeMatrix.length
+        if (this.nodeMatrix && c >= 0 && c < n && r >= 0 && r < n)  this.start = this.nodeMatrix[r][c]
+        else console.log('invalid location')
+        
+    }
+}
+
+////////////////////////////////////////////////////////
+
+class Node {
+    constructor(val,left,right,up,down) {
+        this.val = val
+        this.left = left
+        this.right = right 
+        this.up = up
+        this.down = down        
+        // 
+    }
+    
+}
+// let x = new Matrix
+// x.createMatrixSizeN(3)
+// x.setStartRowColumn(0,0)
+// let z = x.start
+// console.log(z)
+
+let a = new HopCombo()
+// // console.log(a.start)
+let apro = new Date().getTime()
+let b = a.dfsApproach(a.start, 12)
+let bpro = new Date().getTime()
+let c = a.dfsApproachUnique(a.start, 12)
+let cpro = new Date().getTime()
+let d = a.memoizationApproach(a.start, 12)
+let dpro = new Date().getTime()
+let e = a.dpApproach(a.start, 12)
+let epro = new Date().getTime()
+console.log("dfs 4**hops", b, bpro-apro)
+console.log("unique dfs 4**hops", c, cpro-bpro)
+console.log("memoization nodes*hops", d, dpro - cpro)
+console.log("dp nodes*hops", e, epro - dpro)
